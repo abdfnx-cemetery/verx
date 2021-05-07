e2c69bc5edbdcc8218097e0e72571c093808412b
abdfnx <64256993+abdfnx@users.noreply.github.com>
2021-05-07 13:18:46 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 UNAME=$(uname)
-verxUrl=https://raw.githubusercontent.com/abdfnx/verx/HEAD/verx
-verxLocLD=/usr/local/bin
+verxUrl=https://raw.githubusercontent.com/secman-team/verx/HEAD/verx
 
 successInstall() {
     echo "verx was installed successfully 👍"
 }
 
 check_verx_is_installed() {
-    if [ -x "$(command -v verx)"]; then
+    if [ -x "$(command -v verx)" ]; then
         successInstall
     fi
 }
 
 if [ -x "$(command -v curl)" ]; then
-    if [[ "$UNAME" == Linux || "$UNAME" == "Darwin" ]]; then
+    if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]]; then
+        check_verx_is_installed
+
+        wget -P /usr/bin $verxUrl
+
+        check_verx_is_installed
+
+    else
+        verxLocLD=/usr/local/bin
+
         if [ -x "$(command -v sudo)" ]; then
             sudo wget -P $verxLocLD $verxUrl
 
@@ -25,16 +33,5 @@ if [ -x "$(command -v curl)" ]; then
 
             chmod 755 $verxLocLD/verx
         fi
-
-        check_verx_is_installed
-
-    elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]]; then
-        wget -P /usr/bin $verxUrl
-
-        check_verx_is_installed
-
-    else
-        echo "Unidentified system..."
-        echo "support us to 👨‍💻 verx for this system"
     fi
 fi
